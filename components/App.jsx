bc759ca2fb3393560a5c20c756bd188618b6a98e
GURPRIT SINGH <gurprit.iiest@gmail.com>
2020-03-09 15:39:13 +0530

    Update App.jsx

--- components/App.jsx
+++ components/App.jsx
@@ -1,1 +1,18 @@
+import React from "react";
+import Form from "./Form";
+import '../../public/styles.css';
 
+var userIsRegistered = 0;
+
+
+function renderConditionally(userIsRegistered){
+
+return (userIsRegistered?<div className="container"><Form display='hide' submit='Login'/></div>:<div className='container'><Form submit='Register' /></div>);
+
+}
+
+function App() {
+  return renderConditionally(userIsRegistered);
+}
+
+export default App;
