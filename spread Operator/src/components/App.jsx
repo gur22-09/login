cfd1b524e775bd571ef2f2f3336f29f1e5bb5c9b
GURPRIT SINGH <gurprit.iiest@gmail.com>
2020-03-10 13:15:36 +0530

    Add files via upload
    implementation using Spread operator

---
+++ spread Operator/src/components/App.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+function App() {
+  
+  const [lists,setList] =useState('');
+  const [savedList,setsavedList]=useState([]);
+
+  function getList(event){
+    
+    const value =event.target.value;
+    setList(value);
+   
+  
+  }
+  function saveList(){
+    setsavedList(prevList=>{
+      return [...prevList,lists]
+    });
+  }
+
+
+
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={getList} type="text" value={lists} />
+        <button onClick={saveList}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+           {savedList.map(el=>{
+             return <li>{el}</li>
+           })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
